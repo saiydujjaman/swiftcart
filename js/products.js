1e347190a884a7332bcfa4bda8d4cea8f51f44de
saaiydujjaman <saidaronok@gmail.com>
2026-02-17 00:49:08 +0600

    complete all condition

--- js/products.js
+++ js/products.js
@@ -1,3 +1,31 @@
+
+// active or remove
+removeActive = () => {
+    const removeActive = document.querySelectorAll('.removeActive')
+    removeActive.forEach(btn => btn.classList.remove('active'))
+}
+
+
+
+// loader
+const loader = (status) => {
+    if (status == true) {
+        document.getElementById('loader').classList.remove('hidden')
+        document.getElementById('all-products').classList.add('hidden')
+
+    } else {
+        document.getElementById('loader').classList.add('hidden')
+        document.getElementById('all-products').classList.remove('hidden')
+    }
+}
+
+
+const allCategory = () => {
+    const url = fetch('https://fakestoreapi.com/products/categories')
+        .then(res => res.json())
+        .then(data => displayCategory(data))
+}
+
 // product
 const allProduct = () => {
     fetch('https://fakestoreapi.com/products')
@@ -8,13 +36,22 @@ const allProduct = () => {
         })
 }
 
+//category product function
+const categoryProducts = (category) => {
+    loader(true)
+    fetch(`https://fakestoreapi.com/products/category/${category}`)
+        .then(res => res.json())
+        .then(data => displayAllProducts(data))
+
+
+}
+
 // display all product
 const displayAllProducts = (products) => {
     // console.log(products)
     const allProduct = document.getElementById('all-products')
     allProduct.innerHTML = "";
-    const firstThree = products.slice(0, 3)
-    firstThree.forEach(product => {
+    products.forEach(product => {
         const productDiv = document.createElement('div')
         productDiv.innerHTML = `
         <div class="card bg-base-100 shadow-md border">
@@ -61,10 +98,46 @@ const displayAllProducts = (products) => {
                 </div>
         `
         allProduct.append(productDiv)
-
+        loader(false)
     })
 
 
 }
 
 allProduct()
+
+// display Category list
+const displayCategory = (categorys) => {
+
+    const categories = document.getElementById('category')
+    categories.innerHTML = " "
+    categorys.forEach(category => {
+        // console.log(category)
+        const btn = document.createElement('button')
+        btn.className = "btn btn-outline btn-primary px-4 rounded-xl removeActive"
+        btn.innerText = `${category}`
+        btn.addEventListener("click", () => {
+            removeActive()
+            btn.classList.add('active')
+            categoryProducts(`${category}`)
+        })
+        categories.append(btn)
+    })
+}
+
+
+
+
+
+
+allCategory()
+
+
+
+
+
+
+
+
+
+
